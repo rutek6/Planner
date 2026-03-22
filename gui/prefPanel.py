699f57e7bb2fe940d0f34758b9b281f7601b6feb
rutek6 <jan.rutecki15@gmail.com>
2026-03-22 12:18:05 +0100

    modified:   gui/prefPanel.py

--- gui/prefPanel.py
+++ gui/prefPanel.py
@@ -9,18 +9,34 @@ from PySide6.QtWidgets import (QApplication,
                                QHBoxLayout,
                                QVBoxLayout,
                                QLabel,
+                               QFrame,
                                QFormLayout,
                                QGridLayout,
                                QSizePolicy,
                                QSpinBox,
                                QMainWindow,
                                QFileDialog)
+class EntryPanel(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.text = None
+        self.label = QLabel(self.text)
+        self.button = QPushButton("MIAU")
+        layout = QHBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+        
 
 
 class PrefPanel(QWidget):
     def __init__(self):
         super().__init__()
         self.button = QPushButton("Hej")
+        entry1 = EntryPanel()
+        
         layout = QVBoxLayout()
         layout.addWidget(self.button)
+        layout.addWidget(entry1)
         self.setLayout(layout)
+        
